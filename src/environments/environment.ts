4c6ceea0f61937233f6d1fa00001ddaf8e6d97a7
Arfath <arfathabbas.abbas@bt.com>
2025-06-17 11:06:21 +0530

    BE url change after SSL changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    apiBaseUrl: 'https://ec2-13-203-216-77.ap-south-1.compute.amazonaws.com:8080/myFinBud'
+    apiBaseUrl: 'https://myfintracker.duckdns.org'
 };
